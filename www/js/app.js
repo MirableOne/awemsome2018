a5bc5278f54055248639d7c7e5f6d18f91f2a7d0
Pavel Koshelev <koshelev@awem.com>
2018-10-20 16:49:24 +0300

    add user page

--- www/js/app.js
+++ www/js/app.js
@@ -214,6 +214,30 @@ var app = new Framework7({
                 }
             },
         },
+        {
+            path: '/user/',
+            url: './pages/user.html',
+            on: {
+                pageInit: function (e, page) {
+                app.dialog.alert("Hello world");
+                    $$('.convert-form-to-data').on('click', function(){
+                      var formData = app.form.convertToData('#my-form');
+                      alert(JSON.stringify(formData));
+                    });
+
+                    $$('.fill-form-from-data').on('click', function(){
+                      var formData = {
+                        'name': 'John',
+                        'email': 'john@doe.com',
+                        'gender': 'female',
+                        'toggle': ['yes'],
+                      }
+                      app.form.fillFromData('#my-form', formData);
+                    });
+                },
+            }
+        },
+
     ],
 });
 
